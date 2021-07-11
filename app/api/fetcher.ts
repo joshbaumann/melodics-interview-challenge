7d7552f90a52a4e4c5c21213606690f7cbfd852b
Joshua Baumann <lofem123@hotmail.com>
2021-07-11 21:34:34 +1200

    chore: Adding intial cypress deps and setup

--- app/api/fetcher.ts
+++ app/api/fetcher.ts
@@ -1,5 +1,7 @@
 export const fetcher = (uri: string, options?) => {
-  return fetch(`${process.env.NEXT_PUBLIC_HOST}${uri}`, {
+  // Ensures that the SSR is run against localhost in a CI env.
+  const url = process.env.CI ? 'http://localhost:3000' : process.env.NEXT_PUBLIC_HOST;
+  return fetch(`${url}${uri}`, {
     headers: {
       'content-type': 'application/json'
     },
